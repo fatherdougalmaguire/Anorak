15cb8d9df12db7d47ca3b142f649ef83e5241e9d
fatherdougalmaguire <144415904+fatherdougalmaguire@users.noreply.github.com>
2024-08-18 21:25:34 +1000

    Initial Commit

---
+++ Anorak/AnorakApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnorakApp.swift
+//  Anorak
+//
+//  Created by Antonio Sanchez-Rivas on 18/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnorakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Anorak/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Anorak
+//
+//  Created by Antonio Sanchez-Rivas on 18/8/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
